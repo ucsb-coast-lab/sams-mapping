1e46f5683466b1ef658fb4d460447962aa7f6768
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2021-07-26 16:13:13 -0700

    sv added min, max, and mean as well as second file upload

--- pages/map_fn.js
+++ pages/map_fn.js
@@ -1,11 +1,9 @@
+const basemap = new ol.layer.Tile({ source: new ol.source.OSM() })
+
 function createMap(zoom, center){
   return new ol.Map({
     target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
+    layers: [ basemap ],
     view: new ol.View({
       center: center,
       zoom: zoom
@@ -18,6 +16,27 @@ function createLayer(features, style){
     source: new ol.source.Vector({
         features: features
     }),
+    name: 'Data',
     style: style
+  });
+}
+
+function addMinMaxGradient(color1, color2, features, depths){
+  var numberOfItems = (math.max(depths) - math.min(depths));
+  var rainbow = new Rainbow(); 
+  rainbow.setNumberRange(1, numberOfItems);
+  rainbow.setSpectrum(color1, color2);
+  var gradient = [];
+  for (var i = 1; i <= numberOfItems; i++) {
+      var hexColour = rainbow.colourAt(i);
+      gradient.push('#' + hexColour);
+  }
+  features.forEach(feature => {
+    if(parseInt(feature.properties.depth) === Math.min(...depths)){
+      feature.properties.label = "Min: " + feature.properties.depth;
+    }else if(parseInt(feature.properties.depth) === Math.max(...depths)){
+      feature.properties.label = "Max: " + feature.properties.depth;
+    }
+    feature.properties.color = gradient[feature.properties.depth - math.min(depths)];
   });
 }
